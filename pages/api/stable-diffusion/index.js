aae35491262e9f17453015f9cecd203938a36aa4
Connor Rothschild <connorrothschild@gmail.com>
2023-03-07 14:24:16 -0600

    Revert "Misc fixes"
    This reverts commit 43acc5351c8424ef634aefbf656b4b95b995c8cb.

--- pages/api/stable-diffusion/index.js
+++ pages/api/stable-diffusion/index.js
@@ -4,20 +4,19 @@ const API_HOST = "https://api.replicate.com";
 // Very similar to /api/predictions but I didn't want to deal
 // with passing a URL around
 export default async function handler(req, res) {
-  console.log("going to make prediction");
+  console.log('going to make prediction')
 
   const response = await fetch(
-    "https://replicate.com/api/models/stability-ai/stable-diffusion/versions/5b703f0fa41880f918ab1b12c88a25b468c18639be17515259fb66a83f4ad0a4/predictions",
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(req.body),
-    }
-  );
-  console.log("made prediction");
+    "https://replicate.com/api/models/stability-ai/stable-diffusion/versions/5b703f0fa41880f918ab1b12c88a25b468c18639be17515259fb66a83f4ad0a4/predictions", 
+  {
+    method: "POST",
+    headers: {
+      Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req.body),
+  });
+  console.log('made prediction')
 
   if (response.status !== 201) {
     let error = await response.json();
